427c4fe2fca6cff2d47cff31dd0f3ec58e59bf19
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-02-09 22:21:34 +0100

    Upd: Convert to GODOT 3.4.x

--- part04/Godot3_2Dterrain1/Line2D.gd
+++ part04/Godot3_2Dterrain1/Line2D.gd
@@ -3,6 +3,9 @@ extends Line2D
 export var displacement = 100
 export var iterations = 5
 export var height = 250
+export var castlewidth = 175
+export var castleleft = 0
+export var castleright = 0
 export (float) var smooth = 1.1
 var current_displacement
 
@@ -15,14 +18,17 @@ func init_line():
 	current_displacement = displacement
 	var screensize = get_viewport().get_visible_rect().size
 	points = PoolVector2Array()
-	var start = Vector2(0, rand_range(height-displacement,
+	var start = Vector2(castlewidth, rand_range(height-displacement,
 								height+displacement))
-	var end = Vector2(screensize.x, rand_range(height-displacement,
+	var end = Vector2(screensize.x-castlewidth, rand_range(height-displacement,
 								height+displacement))
 	add_point(start)
 	add_point(end)
 	for i in range(iterations):
 		add_points()
+	set_point_position(0,Vector2(0,start.y))
+	set_point_position(1,Vector2(start.x,start.y))
+	add_point(Vector2(screensize.x, end.y))
 	var p = points
 	p.append(Vector2(screensize.x, screensize.y))
 	p.append(Vector2(0, screensize.y))
