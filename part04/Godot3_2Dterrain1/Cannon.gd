94e3f34ff20f1e3e7941ae1f2e45b828ace7073b
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-02-13 16:16:39 +0100

    Add: Variable die bestimmt in welche Richtung die Kannone ausgerichtet werden soll Add: Ausrichtung soll in Ready Func geschehen

--- part04/Godot3_2Dterrain1/Cannon.gd
+++ part04/Godot3_2Dterrain1/Cannon.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+export var left:bool = true
+
 export var muzzle_velocity = 3500
 export var gravity = 250
 
@@ -7,6 +9,12 @@ var can_shoot = true
 
 onready var Bullet = preload("res://Cannonball.tscn")
 onready var Muzzle = get_node("Barrel/Muzzle")
+onready var Barrel = get_node("Barrel")
+
+
+func _ready():
+	## Kannonen umdrehen, wenn auf der rechten Seite
+	pass
 
 func _unhandled_input(event):
 	if event.is_action_released("cannon_shoot") and can_shoot:
@@ -20,8 +28,8 @@ func _unhandled_input(event):
 
 func _process(_delta):
 	if can_shoot:
-		$Barrel.look_at(get_global_mouse_position())
-		$Barrel.rotation_degrees = clamp($Barrel.rotation_degrees, -75, -15)
+		Barrel.look_at(get_global_mouse_position())
+		Barrel.rotation_degrees = clamp(Barrel.rotation_degrees, -75, -15)
 
 
 #func shoot():
