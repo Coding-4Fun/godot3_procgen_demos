13a049b5f4d2ed7daf2d5151b2673f2fdb7f00aa
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-02-13 15:37:16 +0100

    Add: Script für Hauptszene Chg: Ruft die Generierung der TerrainLinie auf Chg: Instanziiert die Kanone und setzt die Position

---
+++ part04/Godot3_2Dterrain1/MainGame.gd
@@ -0,0 +1,56 @@
+extends Node2D
+
+onready var TerrainLine = $"TerrainLine"
+
+onready var castle = preload("res://Castle2d.tscn")
+onready var castleleft = castle.instance()
+onready var castleright = castle.instance()
+
+onready var cannon = preload("res://Cannon.tscn")
+onready var cannonLeft = cannon.instance()
+onready var cannonBarrelLeft = cannonLeft.get_node("Barrel")
+onready var cannonBarrelLeftMuzzle = cannonBarrelLeft.get_node("Muzzle")
+
+onready var cannonRight = cannon.instance()
+onready var cannonBarrelRight = cannonRight.get_node("Barrel")
+onready var cannonBarrelRightMuzzle = cannonBarrelRight.get_node("Muzzle")
+
+onready var line = $TrajectoryLine
+
+
+func _ready():
+	TerrainLine.init_line()
+	#add_Castles()
+	add_cannon_left()
+
+
+func add_cannon_left():
+	cannonLeft.position = TerrainLine.points[0]
+	cannonLeft.position.x = TerrainLine.castlewidth / 2
+	add_child(cannonLeft)
+#	pass
+
+func update_trajectory():
+#	line.clear_points()
+#	var v = barrel.global_transform.x * tank.bullet_speed
+#	var p = muzzle.global_position
+#	for i in num_points:
+#		line.add_point(p)
+#		v.y += tank.gravity * dt
+#		p += v * dt
+#		if p.y > $Ground.position.y - 25:
+#			break
+	pass
+
+func add_Castles():
+	castleleft.position = TerrainLine.points[0]
+	castleleft.modulate = Color.red
+	
+	castleright.position = TerrainLine.points[TerrainLine.points.size()-2]
+	castleright.modulate = Color.green
+	
+	add_child(castleleft)
+	add_child(castleright)
+
+
+
