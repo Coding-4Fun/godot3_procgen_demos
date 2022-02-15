9c72bf8787600436ca14a591a67ba59820ee8df4
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-02-15 18:13:33 +0100

    Add: Übermittlung einer Punktzahl bei Treffer

--- part04/Godot3_2Dterrain1/DummyTarget/DummyTarget.gd
+++ part04/Godot3_2Dterrain1/DummyTarget/DummyTarget.gd
@@ -2,6 +2,8 @@ extends StaticBody2D
 
 signal Hit
 
+export var score : int = 1
+
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
@@ -14,7 +16,7 @@ func _ready() -> void:
 
 func _exit_tree() -> void:
 	print("Dummy exit Tree")
-	emit_signal("Hit")
+	emit_signal("Hit", score)
 	pass
 
 
