9dec72cde7f2eca6178d26c5f06bd6ece989c25d
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-02-15 12:33:05 +0100

    Add: Script für Dummy ===: Signalisiert das es getroffen wurde

---
+++ part04/Godot3_2Dterrain1/DummyTarget/DummyTarget.gd
@@ -0,0 +1,23 @@
+extends StaticBody2D
+
+signal Hit
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+onready var Main = get_tree().get_root().get_node("MainGame")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _exit_tree() -> void:
+	print("Dummy exit Tree")
+	emit_signal("Hit")
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
